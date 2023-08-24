a5158c3c639a3197d353a0e8ac8fab3a44a86650
Vitor Soller <vgsoller@gmail.com>
2023-08-24 15:54:29 -0300

    start Lexer logic

--- src/lexer/Lexer.java
+++ src/lexer/Lexer.java
@@ -1,11 +1,72 @@
 package lexer;
 
+import lexer.tags.Identifier;
+import lexer.tags.Reserved;
+import lexer.tags.Token;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Lexer {
     int columns;
-    boolean isComment;
-    boolean isString;
+    int line;
+    ArrayList<String> lines;
+    private char currentChar = ' ';
 
-    public Lexer(){
-        this.columns = 0;
+    ArrayList<Token> buffer = new ArrayList<>();
+
+    private final HashMap<String, Reserved> reservedTokens = new HashMap<>();
+    private final HashMap<String, Identifier> identifierToken = new HashMap<>();
+
+    final void reserveReserved(Reserved token){
+        this.reservedTokens.put(token.getLexeme(), token);
     }
+
+    final void reserveIdentifier(Identifier token){
+        this.identifierToken.put(token.getLexeme(), token);
+    }
+
+    public Lexer(ArrayList<String> lines){
+        this.columns = 0;
+        this.line = 1;
+        this.lines = lines;
+
+        for(String s : Reserved.reservedWords){
+            this.reserveReserved(new Reserved(s, 0));
+        }
+    }
+
+    public void printLines(){
+        for(String s : this.lines){
+            System.out.println(s);
+        }
+    }
+    public void scanLines(){
+        for(String s : this.lines){
+            this.scanLine(s);
+        }
+    }
+
+    public void scanLine(String line){
+        this.columns = 0;
+        this.line++;
+        this.currentChar = ' ';
+
+        while(this.currentChar != '\n'){
+            this.currentChar = line.charAt(this.columns);
+
+
+            this.columns++;
+        }
+    }
+
+    public void addTokenToBuffer(Token token){
+        this.buffer.add(token);
+    }
+
+
+
+
+
+
 }
--- tests/lexer/TestLexer.java
+++ tests/lexer/TestLexer.java
@@ -1,9 +1,13 @@
 package tests.lexer;
 
 import lexer.Lexer;
+import reader.TxtReader;
+
+import java.util.ArrayList;
 
 public class TestLexer {
     public static void main(String[] args) {
-        Lexer lexer = new Lexer();
+        ArrayList<String> lines = TxtReader.readFile("programa.txt");
+        Lexer lexer = new Lexer(lines);
     }
 }
